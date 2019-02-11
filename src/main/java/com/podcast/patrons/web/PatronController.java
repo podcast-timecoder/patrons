5a78f6b2d3d733de2071df8229ef2814e41a84d3
Alexey Panashchenko <alpa@ciklum.com>
2019-02-11 16:38:00 +0200

    Add PatronList model and remove *.jar from gitignore

--- src/main/java/com/podcast/patrons/web/PatronController.java
+++ src/main/java/com/podcast/patrons/web/PatronController.java
@@ -1,14 +1,20 @@
 package com.podcast.patrons.web;
 
 import com.podcast.patrons.model.Patron;
+import com.podcast.patrons.model.PatronList;
 import com.podcast.patrons.service.PatronService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
+
+import static java.util.Collections.singletonMap;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +23,8 @@ public class PatronController {
     private final PatronService patronService;
 
     @RequestMapping(value = "/patrons", method = RequestMethod.GET)
-    public List<Patron> getAllPatrons() throws IOException {
-        return patronService.getAllPatrons();
+    public PatronList getAllPatrons() throws IOException {
+        List<Patron> patronList = patronService.getAllPatrons();
+        return new PatronList(patronList.size(), patronList);
     }
 }
